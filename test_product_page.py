a10918fd6662132471c67cef40b5adb3897c33a9
psiyavush <125667292+psiyavush@users.noreply.github.com>
2023-08-15 10:27:39 +0300

    Update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -36,7 +36,7 @@ class TestUserAddToBasketFromProductPage:
 
 
 @pytest.mark.need_review
-def test_guest_can_add_product_to_basket(browser, number):
+def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
     page.open()
